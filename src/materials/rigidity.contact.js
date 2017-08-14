e930074a721d4240c8ce7448a999529ce71a0aa5
Sergey Matsitsky <matsitskyss@gmail.com>
2017-08-14 18:04:01 +0300

    change friction materials

--- src/materials/rigidity.contact.js
+++ src/materials/rigidity.contact.js
@@ -5,7 +5,7 @@ export default class RigidityContact {
   constructor (game, materialA, materialB) {
     const contactMaterial = game.physics.p2.createContactMaterial(materialA, materialB)
 
-    contactMaterial.friction = 0.3 // Friction to use in the contact of these two materials.
+    contactMaterial.friction = 0.9 // Friction to use in the contact of these two materials.
     contactMaterial.restitution = 0 // 1.0 // Restitution (i.e. how bouncy it is!) to use in the contact of these two materials.
     contactMaterial.stiffness = 1e7 // (487x16) // Stiffness of the resulting ContactEquation that this ContactMaterial generate.
     contactMaterial.relaxation = 3 // Relaxation of the resulting ContactEquation that this ContactMaterial generate.
